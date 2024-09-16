1349c3dc1b41f8720d11c7955eee0ddf429722a9
Cristian Ortega Hurtado <crisitan.ortegahurtado@ucr.ac.cr>
2024-09-15 21:47:04 -0600

    Modelo para la recuperación de contraseña creado y agregado a la librería.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
 export { ErrorLogInfo } from './src/errorLog/errorLog';
+export { PasswordRecoveryInfo } from './src/passwordRecovery/passwordRecovery';
 export { SessionInfo } from './src/session/session';
 export { UserRole, UserInfo } from './src/user/user';
 /**
@@ -7,6 +8,8 @@ export { UserRole, UserInfo } from './src/user/user';
 export declare enum FirestoreCollections {
     /** Colección de {@link ErrorLogInfo}. */
     ErrorLogInfo = "errorLogInfo",
+    /** Colección de {@link PasswordRecoveryInfo}. */
+    PasswordRecoveryInfo = "passwordRecoveryInfo",
     /** Colección de {@link SessionInfo}. */
     SessionInfo = "sessionInfo",
     /** Colección de {@link UserInfo}. */
---
+++ lib/src/passwordRecovery/passwordRecovery.d.ts
@@ -0,0 +1,14 @@
+/**
+   * Representa la recuperación de contraseña de un usuario ya registrado en el sistema.
+*/
+export interface PasswordRecoveryInfo {
+    /** Correo del usuario para la recuperación de la contraseña. */
+    userEmail: string;
+    /** Contraseña temporal asignada. */
+    temporalPassword: string;
+    /** Fecha y hora de expiración de la contraseña temporal. */
+    passwordExpirationDate: Date;
+    token: string;
+    /** Fecha y hora de expiración del token de autenticación. */
+    tokenExpirationDate: Date;
+}
